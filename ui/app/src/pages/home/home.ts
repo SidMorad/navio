b68eacaf9aafbc1db3221f9b9217fa8a765bc25e
SidMorad <SidMorad@users.noreply.github.com>
2017-05-24 17:03:54 +0430

    Current location updates periodically, in my test device on every ~20 seconds. resolves #4 Show my current location in the Map

--- ui/app/src/pages/home/home.ts
+++ ui/app/src/pages/home/home.ts
@@ -17,35 +17,49 @@ export class HomePage implements OnInit {
     var map = L.map('map', {
       attributionControl: false
     });
+
+    L.tileLayer('http://tile.webebook.org/tile/{z}/{x}/{y}.png', {
+      attribution: 'Rahpey | Map data &copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      attributionPrefix: '',
+      maxZoom: 18
+    }).addTo(map);
+
+    map.addControl(L.control.attribution({
+      position: 'topright',
+      prefix: ''
+    }));
+
+    var markersLayer = new L.LayerGroup([]);
+    markersLayer.addTo(map);
+
+    map.on('click', function(e) {
+      console.log(e);
+    });
+
+    var currentZoom = 18;
+    map.on('zoomend', function(e) {
+      console.log("Zoom end event ", e.target._zoom);
+      currentZoom = e.target._zoom;
+    });
+
     var violetIcon = new L.Icon({
       iconUrl: 'assets/img/marker-icon-2x-violet.png',
-      shadowUrl: 'assets/img/marker-violet.png',
+      shadowUrl: 'assets/img/marker-shadow.png',
       iconSize: [25, 41],
       iconAnchor: [12, 41],
       popupAnchor: [1, -34],
       shadowSize: [41, 41]
     });
-    this.geolocation.getCurrentPosition().then((resp) => {
-      map.setView([resp.coords.latitude, resp.coords.longitude], 18);
 
-      L.marker([resp.coords.latitude, resp.coords.longitude], {icon: violetIcon, draggable: true}).addTo(map);
-      L.tileLayer('http://tile.webebook.org/tile/{z}/{x}/{y}.png', {
-        attribution: 'Rahpey | Map data &copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-        attributionPrefix: '',
-        maxZoom: 18
-      }).addTo(map);
-
-      map.addControl(L.control.attribution({
-        position: 'topright',
-        prefix: ''
-      }));
-
-      map.on('click', function(e) {
-        console.log(e);
-      });
-    }).catch((error) => {
-      console.log("Error on getting current location: ", error);
+    let watchPosition = this.geolocation.watchPosition();
+    watchPosition.subscribe((data) => {
+      console.log("Watch event ", data, " CurrentZoom ", currentZoom);
+      markersLayer.clearLayers();
+      map.setView([data.coords.latitude, data.coords.longitude], currentZoom);
+      var marker = L.marker([data.coords.latitude, data.coords.longitude], {icon: violetIcon, draggable: true});
+      markersLayer.addLayer(marker);
     });
+
   }
 
 }
