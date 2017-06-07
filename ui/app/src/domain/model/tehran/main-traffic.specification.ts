d54fed5c63ea3c46849f3e51706e498fa19945bf
SidMorad <SidMorad@users.noreply.github.com>
2017-06-07 15:38:23 +0430

    Some minor refactorings, plus adding Thursdays logic to TehranMainTrafficSpecification.
    refs #12 Tehran main traffic zone as blocked area

--- ui/app/src/domain/model/tehran/main-traffic.specification.ts
+++ ui/app/src/domain/model/tehran/main-traffic.specification.ts
@@ -5,7 +5,13 @@ export class TehranMainTrafficSpecification {
   isCurrentTimeBetweenForbiddenTime() {
     if (moment().isoWeekday() === 5) {        // Firday, no restriction
       return false;
-    } else {
+    } else if (moment().isoWeekday() === 4){  // Thursday, between 6:30am and 13:00pm
+      let start = moment('6:30am', 'h:mma');
+      let end = moment('13:00pm', 'h:mma');
+      if (moment().isBetween(start, end)) {
+        return true;
+      }
+    } else {                                  // Other days, between 6:30am and 17:00pm
       let start = moment('6:30am', 'h:mma');
       let end = moment('17:00pm', 'h:mma');
       if (moment().isBetween(start, end)) {
@@ -15,4 +21,34 @@ export class TehranMainTrafficSpecification {
     return false;
   }
 
+  static blockedAreaPoints() {
+    return '35.72397,51.38768,35.65780,51.44781';
+  }
+
+  static rectanglePoints() {
+    return [
+      [35.72397, 51.38768],
+      [35.65780, 51.44781]
+    ];
+  }
+
+  static polygonPoints() {
+    return [
+      [35.71452, 51.38897],
+      [35.72118, 51.40765],
+      [35.72404, 51.41006],
+      [35.72465, 51.43761],
+      [35.72407, 51.44107],
+      [35.71779, 51.43946],
+      [35.71731, 51.44085],
+      [35.70698, 51.43485],
+      [35.70710, 51.44101],
+      [35.69958, 51.43884],
+      [35.70202, 51.44871],
+      [35.66032, 51.44612], // طیب
+      [35.65649, 51.40909], // شوش
+      [35.65846, 51.39509], // انتهای کارگر جنوبی
+      [35.71346, 51.38765]  // ابتدای کارگر جنوبی
+    ];
+  }
 }
--- ui/app/src/services/map.service.ts
+++ ui/app/src/services/map.service.ts
@@ -126,7 +126,7 @@ export class MapService {
         if (new TehranMainTrafficSpecification().isCurrentTimeBetweenForbiddenTime()) {
           this.routeControl.getRouter().options.urlParameters = {
             'ch.disable': true,
-            block_area: this.tehranMainTrafficZoneBlockedArea
+            block_area: TehranMainTrafficSpecification.blockedAreaPoints()
           }
         }
         else {
@@ -246,28 +246,11 @@ export class MapService {
     shadowSize: [41, 41]
   });
 
-  tehranMainTrafficZoneBlockedArea = '35.72397,51.38768,35.65780,51.44781';
   tehranMainTrafficZoneRectangle = L.rectangle([
-    [35.72397, 51.38768],
-    [35.65780, 51.44781]
+    TehranMainTrafficSpecification.rectanglePoints()
   ]);
-  tehranMainTrafficZonePolygon = L.polygon([
-    [35.71452, 51.38897],
-    [35.72118, 51.40765],
-    [35.72404, 51.41006],
-    [35.72465, 51.43761],
-    [35.72407, 51.44107],
-    [35.71779, 51.43946],
-    [35.71731, 51.44085],
-    [35.70698, 51.43485],
-    [35.70710, 51.44101],
-    [35.69958, 51.43884],
-    [35.70202, 51.44871],
-    [35.66032, 51.44612], // طیب
-    [35.65649, 51.40909], // شوش
-    [35.65846, 51.39509], // انتهای کارگر جنوبی
-    [35.71346, 51.38765], // ابتدای کارگر جنوبی
-  ], {
+  tehranMainTrafficZonePolygon = L.polygon(
+    TehranMainTrafficSpecification.polygonPoints(), {
     color: 'red',
     fillColor: 'pink',
     fillOpacity: 0.2
