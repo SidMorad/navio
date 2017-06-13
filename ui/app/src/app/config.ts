30967ae778b92a364ba46183ac631d247d1ded4f
SidMorad <SidMorad@users.noreply.github.com>
2017-06-13 23:12:26 +0430

    We are using our own Geocoding API endpoint as of this revision.
    So we must not be worried about facing usage limit by Nominatim.openstreetmap.org server anymore! :-)
    
    resolves #11 set destination

--- ui/app/src/app/config.ts
+++ ui/app/src/app/config.ts
@@ -1,4 +1,4 @@
 export let ROUTE_API_BASE_URL = 'http://tile.webebook.org/route/v1/get';
 export let TILE_API_BASE_URL = 'http://tile.webebook.org/tile';
-export let GEOCODING_API_BASE_URL = 'http://nominatim.openstreetmap.org/';
+export let GEOCODING_API_BASE_URL = 'http://tile.webebook.org/geocoding/';
 export let OVERPASS_API_BASE_URL = 'http://tile.webebook.org/overpass/';
