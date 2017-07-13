912d095b59a47012d073da764ae5ec68e5103393
SidMorad <SidMorad@users.noreply.github.com>
2017-07-13 09:43:41 +0430

    OverPassLayer library patched, sent requests are more cachable as of this  revision. In another words sent requests have more chance to hit the cache.
    refs #14 Display speed cameras on the map.

--- ui/app/sign.sh
+++ ui/app/sign.sh
@@ -1,1 +1,8 @@
+#!/bin/sh
+
+# First patch OverPassLayer library
+cp patchs/OverPassLayer-patched.js node_modules/leaflet-overpass-layer/src/OverPassLayer.js
+
+echo '' > node_modules/leaflet-overpass-layer/src/OverPassLayer.css
+
 ionic cordova build android --prod --release --buildConfig
