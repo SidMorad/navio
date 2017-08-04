42f5589ad81cc41acb878f88d79763529f40983e
Babak <babak.taraghi@gmail.com>
2017-08-04 21:54:07 +0800

    ios project init

---
+++ ui/ios/Navio/FirstViewController.swift
@@ -0,0 +1,27 @@
+//
+//  FirstViewController.swift
+//  Navio
+//
+//  Created by Babak on 04/08/2017.
+//  Copyright © 2017 navio tech team. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+
+		// TODO: load map
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
