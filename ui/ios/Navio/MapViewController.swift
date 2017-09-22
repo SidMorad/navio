0dd04ae65dbc0903bbbd638aa7dd02745c76d386
Babak <babak.taraghi@gmail.com>
2017-09-22 14:46:31 +0800

    Loading map tiles from Navio dev server using Mapbox framework

--- ui/ios/Navio/MapViewController.swift
+++ ui/ios/Navio/MapViewController.swift
@@ -7,21 +7,36 @@
 //
 
 import UIKit
-import MapKit
+import Mapbox
 
 class MapViewController: UIViewController {
 
-	@IBOutlet var mapViewOutlet: MKMapView!
-
+	var mapView: MGLMapView!
 	let locationManager = CLLocationManager()
+
+	// TODO: temporary, needs to be replaced by user current location coordinates
+	// Tehran coordinates
+	let customeLocation : CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: 35.6892, longitude: 51.389)
+
+
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-
 		requestLocationAccess()
 
-		mapViewOutlet?.showsUserLocation = true
+		let styleURL = URL(string: "navioRasterV8.json")
+		mapView = MGLMapView(frame: view.bounds, styleURL: styleURL)
+
+		//locationManager.location!.coordinate
+		mapView.setCenter(customeLocation, zoomLevel: 10, animated: false)
+
+		mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+		mapView.tintColor = .white
+
+		view.addSubview(mapView)
 	}
+
+
 
 
 	func requestLocationAccess() {
@@ -38,6 +53,8 @@ class MapViewController: UIViewController {
 			locationManager.requestAlwaysAuthorization()
 		}
 	}
+
+
 
 	
 	override func didReceiveMemoryWarning() {
