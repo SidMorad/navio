9575d578fe831b6b39f17e9a2e216cc1ce5bb322
Babak <babak.taraghi@gmail.com>
2017-10-08 11:03:16 +0800

    Patch update
    - Regarding Solar lib update (2.1.0)
    
    https://github.com/ceeK/Solar

--- ui/ios/Patches/NavigationViewController.swift
+++ ui/ios/Patches/NavigationViewController.swift
@@ -279,7 +279,7 @@ public class NavigationViewController: NavigationPulleyViewController, RouteMapV
         guard automaticallyAdjustsStyleForTimeOfDay else { return .dayStyle }
         
         guard let location = routeController.location,
-            let solar = Solar(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude),
+			let solar = Solar(coordinate: location.coordinate),
             let sunrise = solar.sunrise, let sunset = solar.sunset else {
                 return .dayStyle
         }
